706620ac849524a4b16024752d199ac3b801bb11
Dennis Barzanoff <denis.barzanov2002@gmail.com>
2025-05-22 17:32:56 +0000

    feat: add docs and products collection

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,19 @@
-module.exports = () => ({});
+module.exports = () => ({
+  'documentation': {
+    enabled: true,
+    config: {
+      openapi: '3.0.0',
+      info: {
+        version: '1.0.0',
+        title: 'Strapi API Documentation',
+        description: 'Documentation for the Strapi API',
+      },
+      servers: [
+        {
+          url: 'http://localhost:1337',
+          description: 'Development server',
+        },
+      ],
+    },
+  },
+});
