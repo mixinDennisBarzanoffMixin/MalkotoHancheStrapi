cce48a17d62a18857a69cdaf99a8bde2e141e020
Dennis Barzanoff <denis.barzanov2002@gmail.com>
2025-06-20 21:07:33 +0000

    fix: remove permissions and update packages

--- src/extensions/users-permissions/config/policies/permissions.js
+++ src/extensions/users-permissions/config/policies/permissions.js
@@ -1,44 +1,6 @@
 'use strict';
 
 module.exports = async (ctx, next) => {
-  // Set default permissions for public role
-  const publicRole = await strapi.query('plugin::users-permissions.role').findOne({
-    where: { type: 'public' },
-  });
-
-  if (publicRole) {
-    // Explicitly check for specific permissions
-    const requiredPermissions = [
-      'api::product.product.find',
-      'api::product.product.findOne',
-      'api::category.category.find',
-      'api::category.category.findOne',
-      'api::recipe.recipes.find',
-      'api::recipe.recipesfindOne',
-    ];
-
-    const existingPermissions = await strapi.query('plugin::users-permissions.permission').findMany({
-      where: { 
-        role: publicRole.id,
-        action: { $in: requiredPermissions }
-      },
-    });
-
-    // Create missing permissions
-    const existingActions = existingPermissions.map(p => p.action);
-    console.log(existingActions);
-    const missingPermissions = requiredPermissions.filter(action => !existingActions.includes(action));
-    console.log(missingPermissions);
-
-    for (const action of missingPermissions) {
-      await strapi.query('plugin::users-permissions.permission').create({
-        data: {
-          action,
-          role: publicRole.id,
-        },
-      });
-    }
-  }
-
+  // Minimal permissions policy - just continue to next middleware
   return next();
 }; 
