7c2ecf5479f688458ab30db1cdcd136ea90ce6d1
mixinDennisBarzanoffMixin <102508565+mixinDennisBarzanoffMixin@users.noreply.github.com>
2025-08-24 21:00:13 +0000

    feat: fix: twillio and stop leaking data

--- src/api/order/services/order.js
+++ src/api/order/services/order.js
@@ -11,14 +11,14 @@ module.exports = createCoreService('api::order.order', ({ strapi }) => ({
     
     try {
       // E-Mail an den Kunden und Besitzer senden
-      console.log('Sending owner email to denis.barzanov2002@gmail.com');
-      await sendOwnerEmail(order, 'denis.barzanov2002@gmail.com');
+      console.log('Sending owner email to', process.env.OWNER_EMAIL_1);
+      await sendOwnerEmail(order, process.env.OWNER_EMAIL_1);
       
-      console.log('Sending owner email to ivankageorgievablagoeva@abv.bg'); 
-      await sendOwnerEmail(order, 'ivankageorgievablagoeva@abv.bg');
+      console.log('Sending owner email to', process.env.OWNER_EMAIL_2); 
+      await sendOwnerEmail(order, process.env.OWNER_EMAIL_2);
 
-      console.log('Sending owner email to info@malkotohanche.com');
-      await sendOwnerEmail(order, 'info@malkotohanche.com');
+      console.log('Sending owner email to', process.env.OWNER_EMAIL_3);
+      await sendOwnerEmail(order, process.env.OWNER_EMAIL_3);
 
       if (order.customerEmail) {
         console.log('Sending customer email to', order.customerEmail);
@@ -27,8 +27,8 @@ module.exports = createCoreService('api::order.order', ({ strapi }) => ({
         console.log('No customer email provided, skipping customer notification');
       }
 
-      console.log('Sending SMS notification to +35979399088');
-      await sendTwillio(order, '+35979399088');
+      console.log('Sending SMS notification to', process.env.TWILIO_TO_PHONE_NUMBER);
+      await sendTwillio(order, process.env.TWILIO_TO_PHONE_NUMBER);
       
       strapi.log.info('Successfully sent all notifications for order');
     } catch (error) {
